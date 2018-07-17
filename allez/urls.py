1b5495dcff1045a2e0652d4d1233e213774a3a5a
Kyle Cooke <cookekyle99@yahoo.co.uk>
2018-07-17 09:26:30 +0100

    add authentication urls

--- allez/urls.py
+++ allez/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include('main.urls')),
-    path('ui/', include('ui.urls'))
+    path('ui/', include('ui.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
