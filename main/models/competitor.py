62f8289bd96d0f490d130e9117c3c5d5712318fa
kyle <cookekyle97@gmail.com>
2018-07-19 22:01:53 +0100

    modify competitor class
    remove country as irrelevant at national level and club is irrelevant at international level
    
    add organisation as organisations can't be allowed to see data of other organisations competitors

--- main/models/competitor.py
+++ main/models/competitor.py
@@ -5,6 +5,6 @@ from . import Club
 
 class Competitor(models.Model):
     name = models.CharField(max_length=100)
+    organisation = models.ForeignKey('main.Organisation', on_delete=models.CASCADE)
     license_number = models.CharField(max_length=15)
     club = models.ForeignKey(Club, on_delete=models.SET_NULL, null=True)
-    country = models.CharField(max_length=3, choices=country_list, default='GBR')
