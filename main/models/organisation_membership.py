7eb8e8cd1f648bf760bd8c7dd28bcf5aac3faeae
kyle <cookekyle97@gmail.com>
2018-07-31 11:50:41 +0100

    add a state field to organisation membership model

--- main/models/organisation_membership.py
+++ main/models/organisation_membership.py
@@ -4,6 +4,13 @@ from . import Organisation
 
 
 class OrganisationMembership(models.Model):
+    MANAGER = 'MG'
+    DT = 'DT'
+    APPLICANT = "AP"
+    states = ((MANAGER, "Manager"),
+              (DT, "DT"),
+              (APPLICANT, "AP"))
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     organisation = models.ForeignKey(Organisation, on_delete=models.CASCADE)
     last_active = models.DateTimeField(auto_now_add=True)
+    state = models.CharField(choices=states, max_length=2, default=APPLICANT)
--- main/rules.py
+++ main/rules.py
@@ -3,7 +3,10 @@ from main import models
 
 
 def is_dt(user, competition):
-    return models.OrganisationMembership.objects.filter(organisation=competition.organisation, user=user).exists()
+    org_mem = models.OrganisationMembership
+    dt_states = [org_mem.MANAGER, org_mem.DT]
+    return models.OrganisationMembership.objects.\
+        filter(organisation=competition.organisation, user=user, state__in=dt_states).exists()
 
 
 @rules.predicate
@@ -35,9 +38,13 @@ def is_dt_de_table_entry(user, de_entry):
 @rules.predicate
 def is_manager(user, test_object):
     if test_object.__class__ == models.Organisation:
-        return models.OrganisationMembership.objects.filter(organisation=test_object, user=user).exists()
+        return models.OrganisationMembership.objects.\
+            filter(organisation=test_object, user=user, state=models.OrganisationMembership.MANAGER).\
+            exists()
     elif test_object.__class__ == models.Competition:
-        return models.OrganisationMembership.objects.filter(organisation=test_object.organisation, user=user).exists()
+        return models.OrganisationMembership.objects.\
+            filter(organisation=test_object.organisation, user=user, state=models.OrganisationMembership.MANAGER).\
+            exists()
     raise ValueError('Unexpected object: ', test_object)
 
 
@@ -45,3 +52,4 @@ rules.add_perm('main.change_pool', is_referee_for_pool | is_dt_pool)
 rules.add_perm('main.change_de_table_entry', is_referee_for_de | is_dt_de_table_entry)
 rules.add_perm('main.create_competition', is_manager)
 rules.add_perm('main.manage_competition', is_manager)
+rules.add_perm('main.manage_organisation', is_manager)
