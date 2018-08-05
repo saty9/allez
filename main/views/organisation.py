83fd37cda701a79a1b3e24f16ffab1f90e6ca6ae
kyle <cookekyle97@gmail.com>
2018-08-05 12:28:37 +0100

    add the ability to accept organisation membership applications from an Organisations page

--- main/views/organisation.py
+++ main/views/organisation.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from main.helpers.permissions import permission_required_json, direct_object
 from main.models import Organisation, OrganisationMembership
 
 
@@ -11,8 +12,10 @@ def organisation(request, org_id):
         r_type = request.POST['type']
         if r_type == "join_request":
             return join_organisation(request, org)
+        elif r_type == "accept_application":
+            return accept_application(request, org)
     return JsonResponse({'success': False,
-                         'reason': 'Unrecognised response'})
+                         'reason': 'Unrecognised request'})
 
 
 @login_required
@@ -25,6 +28,24 @@ def join_organisation(request, org):
         else:
             return JsonResponse({'success': False,
                                  'reason': 'User is already a member'})
+
     else:
         return JsonResponse({'success': False, 'reason': 'InsufficientPermissions'}, status=403)
+
+
+@login_required
+@permission_required_json('main.manage_organisation', fn=direct_object)
+def accept_application(request, org):
+    """Allows an organisation manager to accept applications to join an org
+    expects 'user_id' in POST to be the pk of the Organisation membership to accept"""
+    user_id = request.POST['user_id']
+    membership = get_object_or_404(OrganisationMembership, user_id=user_id, organisation=org)
+    if membership.state == OrganisationMembership.APPLICANT:
+        membership.state = OrganisationMembership.DT
+        membership.save()
+        return JsonResponse({'success': True})
+    else:
+        return JsonResponse({'success': False,
+                             'reason': "User is already a full member"})
+
 
