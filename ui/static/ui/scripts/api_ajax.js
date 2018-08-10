f2fd749bf1d6fb69a2c2f97f480ee00007d01029
kyle <cookekyle97@gmail.com>
2018-08-10 18:30:15 +0100

    add js function for performing ajax requests to the api

---
+++ ui/static/ui/scripts/api_ajax.js
@@ -0,0 +1,48 @@
+function api_post(target, data, success) {
+    return api_post(target,data,success, null, null)
+}
+
+function api_post(target, data, success, failure) {
+    return api_post(target,data,success, failure, null)
+}
+
+function api_post(target, data, success, failure, error){
+    let csrftoken = $("[name=csrfmiddlewaretoken]").val();
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }});
+    $.ajax({
+        type:'POST',
+        url: target,
+        data: data,
+        dataType: 'json',
+        success: function (data) {
+            if (data['success']) {
+                success(data);
+            } else {
+                console.log(data);
+                $('#alerts').innerText = "";
+
+                if (failure){
+                    failure(data)
+                } else {
+                    $('#alerts').append('<div class="alert alert-failure">' + data['verbose_reason'] + '</div>')
+                }
+            }},
+        error: function (data) {
+            console.log(data);
+            if (error){
+                error(data);
+            } else {
+                if (data.status == 403){
+                    $('#alerts').innerText = "";
+                    $('#alerts').append(
+                        '<div class="alert alert-failure">You don\'t have permission to do that please check you are"+ ' +
+                        'logged in and can perform this action</div>'
+                    )
+                }
+            }
+        }
+    });
+}
