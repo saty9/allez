94bbedcce208db0d45b3a668bebc9f436755d001
kyle <cookekyle97@gmail.com>
2018-08-17 09:08:19 +0100

    fix bug in api_failure

--- main/utils/api_responses.py
+++ main/utils/api_responses.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 
 def api_failure(reason, verbose_reason=None):
     if not verbose_reason:
-        verbose_reason = verbose_reason
+        verbose_reason = reason
     return JsonResponse({'success': False,
                          'reason': reason,
                          'verbose_reason': verbose_reason})
