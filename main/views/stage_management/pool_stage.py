4c403a3ec495aba4a8201b72cff92d906e207bd0
kyle <cookekyle97@gmail.com>
2018-08-28 23:28:48 +0100

    refactor pool_stage api

--- main/views/stage_management/pool_stage.py
+++ main/views/stage_management/pool_stage.py
@@ -24,67 +24,59 @@ def manage_pool_stage(request, stage):
 def generate_pools(request, stage):
     pool_stage = stage.poolstage_set.first()
     previous = Stage.objects.get(competition=stage.competition, number=stage.number - 1)
-    if stage.state == stage.NOT_STARTED:
-        if previous.state in [Stage.FINISHED, Stage.LOCKED]:
-            number = int(request.POST['number_of_pools'])
-            entry_count = len(previous.ordered_competitors())
-            if entry_count / number >= 3.0 and ceil(entry_count / number) <= MAX_POOL_SIZE:
-                pool_stage.start(number)
-                stage.state = Stage.READY
-                stage.save()
-                return api_success()
-            else:
-                return api_failure("invalid pool size",
-                                   _('produces pools with less than 3 or more than ') + str(MAX_POOL_SIZE) + _(' fencers'))
-        else:
-            return api_failure('previous stage not completed yet')
-    else:
+    if stage.state != stage.NOT_STARTED:
         return api_failure("incorrect state",
                            _('this stage has already generated pools'))
+    if previous.state not in [Stage.FINISHED, Stage.LOCKED]:
+        return api_failure('previous stage not completed yet')
+    number = int(request.POST['number_of_pools'])
+    entry_count = len(previous.ordered_competitors())
+    if entry_count / number >= 3.0 and ceil(entry_count / number) <= MAX_POOL_SIZE:
+        pool_stage.start(number)
+        stage.state = Stage.READY
+        stage.save()
+        return api_success()
+    else:
+        return api_failure("invalid pool size",
+                           _('produces pools with less than 3 or more than ') + str(MAX_POOL_SIZE) + _(' fencers'))
 
 
 @permission_required_json('main.manage_competition', fn=direct_object)
 def confirm_pools(request, stage):
     pool_stage = stage.poolstage_set.first()
-    if stage.state == Stage.READY:
-        if 'pools' in request.POST:
-            original_pools = pool_stage.pool_set.all()
-            pools = json.loads(request.POST['pools'])
-            if len(pools) == len(original_pools):
-                original_competitors = set()
-                [original_competitors.update(x.poolentry_set.all()) for x in original_pools]
-                original_competitors_ids = set(map(lambda x: x.entry_id, original_competitors))
-                competitors_ids = set()
-                new_lengths = []
-                for pool_num, entries in pools.items():
-                    competitors_ids.update(map(int, entries))
-                    new_lengths.append(len(entries))
-                if competitors_ids == original_competitors_ids:
-                    if max(new_lengths) <= min(new_lengths) + 1:
-                        original_pools.delete()
-                        for index, pool in enumerate(pools.items()):
-                            new_pool = pool_stage.pool_set.create(number=index+1)
-                            for index, entry_id in enumerate(pool[1]):
-                                new_pool.poolentry_set.create(number=index+1, entry_id=int(entry_id))
-                        stage.state = Stage.STARTED
-                        stage.save()
-                        return api_success()
-                    else:
-                        return api_failure("pool size difference",
-                                           _("Pools can differ in size by at most 1"))
-                else:
-                    return api_failure("pool entry mismatch",
-                                       _("Manipulation of entry ids detected"))
-            else:
-                return api_failure("pool count mismatch",
-                                   _("Number of pools submitted does not match number of pools in this round"))
-        else:
-            stage.state = Stage.STARTED
-            stage.save()
-            return api_success()
-    else:
+    if stage.state != Stage.READY:
         return api_failure("incorrect state",
                            _("Stage not in ready state"))
+    if 'pools' not in request.POST:
+        stage.state = Stage.STARTED
+        stage.save()
+        return api_success()
+    original_pools = pool_stage.pool_set.all()
+    pools = json.loads(request.POST['pools'])
+    if len(pools) != len(original_pools):
+        return api_failure("pool count mismatch",
+                           _("Number of pools submitted does not match number of pools in this round"))
+    original_competitors_ids = set()
+    [original_competitors_ids.update(x.poolentry_set.values_list('entry_id', flat=True)) for x in original_pools]
+    competitors_ids = set()
+    new_lengths = []
+    for pool_num, entries in pools.items():
+        competitors_ids.update(map(int, entries))
+        new_lengths.append(len(entries))
+    if competitors_ids != original_competitors_ids:
+        return api_failure("pool entry mismatch",
+                           _("Manipulation of entry ids detected"))
+    if not (max(new_lengths) <= min(new_lengths) + 1):
+        return api_failure("pool size difference",
+                           _("Pools can differ in size by at most 1"))
+    original_pools.delete()
+    for index, pool in enumerate(pools.items()):
+        new_pool = pool_stage.pool_set.create(number=index+1)
+        for index, entry_id in enumerate(pool[1]):
+            new_pool.poolentry_set.create(number=index+1, entry_id=int(entry_id))
+    stage.state = Stage.STARTED
+    stage.save()
+    return api_success()
 
 
 @permission_required_json('main.manage_competition', fn=direct_object)
