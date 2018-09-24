8ab192b49123a715111d975b1018ee8f430b3688
Kyle Cooke <cookekyle99@yahoo.co.uk>
2018-09-24 22:47:11 +0100

    add admin interface for managing organisations

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Organisation, OrganisationMembership, Address
 
-# Register your models here.
+
+class OrganisationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+
+
+class OrganisationMembershipAdmin(admin.ModelAdmin):
+    list_display = ('user', 'organisation', 'state')
+
+
+admin.site.register(Organisation, OrganisationAdmin)
+admin.site.register(OrganisationMembership, OrganisationMembershipAdmin)
+admin.site.register(Address)
