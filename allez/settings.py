52c089c6065ad2e4a27091427d7ec5e553ef9798
kyle <cookekyle97@gmail.com>
2021-05-08 12:28:25 +0100

    add pipfile and fix broken imports

--- allez/settings.py
+++ allez/settings.py
@@ -78,12 +78,11 @@ WSGI_APPLICATION = 'allez.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'OPTIONS': {
+            'timeout': 20,
+        }
     }
 }
 
